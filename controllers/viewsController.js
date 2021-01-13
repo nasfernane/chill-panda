522e5a088ca138b04c9013ad4f9eb70d68f26c26
nasfernane <nasfernane@gmail.com>
2021-01-13 12:40:27 +0100

    first template overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,16 @@
+const Project = require('../models/projectModel');
+const catchAsync = require('../utils/catchAsync');
+
 // overview de tous les projets
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res) => {
+    // récupération de tous les projets projets de la collection
+    const projects = await Project.find();
+
     res.status(200).render('overview', {
         title: 'All Projects',
+        projects,
     });
-};
+});
 
 // page projet individuel
 exports.getProject = (req, res) => {
