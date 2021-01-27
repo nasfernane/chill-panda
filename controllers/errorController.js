fc829f84b4d33f442d63afb06dacbd27abcfde2f
nasfernane <nasfernane@gmail.com>
2021-01-27 15:53:53 +0100

    fix mineur

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -61,7 +61,7 @@ const sendErrorProd = (err, req, res) => {
         if (err.isOperational) {
             return res.status(err.statusCode).json({
                 status: err.status,
-                message: 'err.message',
+                message: err.message,
             });
         }
 
